dc8e8594df7efa10cd14e091eaa487d3befec451
long.gf <long.gf@topsports.com.cn>
2023-05-29 16:25:54 +0800

    init by https://start.aliyun.com/

---
+++ src/main/java/com/lgfei/osio/register/OsioRegisterApplication.java
@@ -0,0 +1,13 @@
+package com.lgfei.osio.register;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsioRegisterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OsioRegisterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lgfei/osio/register/OsioRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lgfei.osio.register;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsioRegisterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
